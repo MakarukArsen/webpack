ce618961429844af158a8bf7a49e02af04efa662
MakarukArsen <makarukarsen@gmail.com>
2022-07-07 17:32:56 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src/script.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        port: 1234,
+        open: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|svg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ]
+    }
+};
